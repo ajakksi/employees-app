e1b59c597752b90371fe0042eab439dd247daae6
Ksenia <a.jakksi27@gmail.com>
2026-04-29 19:16:26 +0200

    Add project template: layout and structure

---
+++ src/components/app-filter/app-filter.jsx
@@ -0,0 +1,22 @@
+import './app-filter.css';
+
+const AppFilter = () => {
+    return(
+        <div className="btn-group">
+            <button className="btn btn-light"
+            type = 'button'>
+                All employees
+            </button>
+            <button className="btn btn-outline-light"
+            type = 'button'>
+                For a pay rise
+            </button>
+            <button className="btn btn-outline-light"
+            type = 'button'>
+                Salary over $2,000
+            </button>
+        </div>
+    );
+}
+
+export default AppFilter;
---
+++ src/components/app-info/app-info.jsx
@@ -0,0 +1,14 @@
+import './app-info.css';
+
+const AppInfo = () =>{
+return(
+    <div className="app-info">
+        <h1>Staff records within the company</h1>
+        <h2>Total number of workers: </h2>
+        <h2>The award will be presented to: </h2>
+    </div>
+)
+
+}
+
+export default AppInfo;
